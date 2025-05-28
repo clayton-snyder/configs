6e4e59f681a09273a464a7229d30ca821306e9d5
Clayton <snyderc@gmx.com>
2025-05-28 16:15:05 -0700

    disabling centering on CTRL+d/u for now

--- nvim/init.lua
+++ nvim/init.lua
@@ -47,8 +47,8 @@ vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- vim.keymap.set("n", "<C-d>", "<C-d>zz")
+-- vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
